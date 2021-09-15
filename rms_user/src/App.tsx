24d77a34ba4ef0251abb49088a964cadaa930d0d
hyejun <hyejun0608@naver.com>
2021-09-15 22:09:16 +0900

    :gear: :: (#0) 프로젝트 세팅

--- rms_user/src/App.tsx
+++ rms_user/src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyle from './style/globalStyle';
+import MainRouter from './route/MainRouter'; 
+import { Provider } from 'react-redux';
+import store from './modules/redux/store';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <GlobalStyle />
+      <MainRouter />
+    </Provider>
   );
-}
+};
 
 export default App;
