193772d232e2f54746a4fdf0243eef0287d60a29
kky04994 <kkyo8747@naver.com>
2021-09-20 04:02:34 +0900

    ⚡️ :: (#9) mainrouter add mypage

--- rms_user/src/route/MainRouter.tsx
+++ rms_user/src/route/MainRouter.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { LoginContainer } from '../containers';
+import { LoginContainer, MyPageContainer } from '../containers';
 
 const MainRouter = () => {
   return (
     <BrowserRouter>
       <Switch>
-          <Route exact path='/login' component={LoginContainer} />
+        <Route exact path='/login' component={LoginContainer} />
+        <Route exact path='/mypage' component={MyPageContainer} />
       </Switch>
     </BrowserRouter>
   );
