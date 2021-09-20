fdfc9a4ba040542a788ebcba0b4bcde81d60b5d0
kky04994 <kkyo8747@naver.com>
2021-09-20 12:56:44 +0900

    ⚡️ :: (#2) Header component

---
+++ rms_user/src/components/header/style/index.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { color } from '../../../style';
+
+export const HeaderBox = styled.div``;
+
+export const HeaderContent = styled.div``;
+
+export const MainLogo = styled.div``;
+
+export const UserBox = styled.div``;
+
+export const UserName = styled.span``;
