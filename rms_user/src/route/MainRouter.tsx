1b1a3a1d8f4df35fbabe5fdc5ab03e53f7290cb9
hyejun <hyejun0608@naver.com>
2021-09-23 01:53:14 +0900

    ⚡️ :: (#2) MainRouter에 MainContainer 추가

--- rms_user/src/route/MainRouter.tsx
+++ rms_user/src/route/MainRouter.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { LoginContainer } from '../containers';
+import { LoginContainer, MainContainer } from '../containers';
 
 const MainRouter = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path='/login' component={LoginContainer} />
+        <Route exact path='/' component={MainContainer} />
       </Switch>
     </BrowserRouter>
   );
