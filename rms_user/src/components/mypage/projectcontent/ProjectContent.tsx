0540a699f1fdfba0bcc6ea9623292e88259ea3e7
kky04994 <kkyo8747@naver.com>
2021-09-23 03:49:53 +0900

    ⚡️ :: (#9) projectcontent style 추가

--- rms_user/src/components/mypage/projectcontent/ProjectContent.tsx
+++ rms_user/src/components/mypage/projectcontent/ProjectContent.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { FC } from 'react';
 import * as S from './style';
 
-const ProjectContent = () => {
-  return <></>;
+interface Props {
+  title: string;
+  classification: string;
+  writer: string;
+  field: Array<string>;
+}
+
+const ProjectContent: FC<Props> = ({ classification, title, writer, field }) => {
+  return (
+    <S.ProjectContent>
+      <S.ProjectClassification>[{classification}]</S.ProjectClassification>
+      <S.ProjectTitle>{title}</S.ProjectTitle>
+      <S.ProjectWriter>{writer}</S.ProjectWriter>
+      <div>
+        {field.map(content => {
+          return (
+            <S.ProjectField>
+              <p>{content}</p>
+            </S.ProjectField>
+          );
+        })}
+      </div>
+    </S.ProjectContent>
+  );
 };
 
 export default ProjectContent;
