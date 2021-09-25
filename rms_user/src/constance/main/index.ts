e946ca927f8fd11b15ba531b05634df73c74d2c9
hyejun <hyejun0608@naver.com>
2021-09-25 17:03:37 +0900

    ⚡️ :: (#2) type 추가

--- rms_user/src/constance/main/index.ts
+++ rms_user/src/constance/main/index.ts
@@ -1,5 +1,5 @@
 export const CHOOSEFIELDTITLE = '분야 선택';
-export const FIELDS = [
+export const FIELDS: Array<FieldMapPropsType> = [
   { id: 'web', content: '웹' },
   { id: 'app', content: '앱' },
   { id: 'game', content: '게임' },
@@ -8,11 +8,18 @@ export const FIELDS = [
   { id: 'aiAndBigData', content: '인공지능/빅데이터' },
 ];
 
-export type CheckState = {
+export type CheckStateType = {
   web: boolean;
   app: boolean;
   game: boolean;
   embedded: boolean;
   security: boolean;
   aiAndBigData: boolean;
+};
+
+export type DataIdType = 'web' | 'app' | 'game' | 'embedded' | 'security' | 'aiAndBigData';
+
+export type FieldMapPropsType = {
+  id: DataIdType;
+  content: string;
 };
