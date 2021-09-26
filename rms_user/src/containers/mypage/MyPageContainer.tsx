c60f425b81f78f146241e61c48b62ca384dd0899
kky04994 <kkyo8747@naver.com>
2021-09-27 03:47:23 +0900

    ⚡️ :: (#9) MyPageContainer

--- rms_user/src/containers/mypage/MyPageContainer.tsx
+++ rms_user/src/containers/mypage/MyPageContainer.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { FC, Suspense } from 'react';
 import MyPage from '../../components/mypage';
 
-const MyPageContainer = () => {
-  return <MyPage />;
+const MyPageContainer: FC = () => {
+  return (
+    <Suspense fallback={<div>Loading</div>}>
+      <MyPage />
+    </Suspense>
+  );
 };
 
 export default MyPageContainer;
