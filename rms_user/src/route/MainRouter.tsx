73d86fb05b4d71f2e4e11b828f0b1633097db4f8
hyejun <hyejun0608@naver.com>
2021-09-27 01:31:54 +0900

    ⚡️ :: (#5) detail-report router 추가

--- rms_user/src/route/MainRouter.tsx
+++ rms_user/src/route/MainRouter.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { LoginContainer } from '../containers';
+import { LoginContainer, DetailReportContainer } from '../containers';
 
 const MainRouter = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path='/login' component={LoginContainer} />
+        <Route exact path='/detail-report/:id' component={DetailReportContainer} />
       </Switch>
     </BrowserRouter>
   );
