dd41c9d06e9970c8fc80aff524ec50196fa001cc
hyejun <hyejun0608@naver.com>
2021-09-27 01:31:20 +0900

    ⚡️ :: (#5) 추가적인 constance 추가

--- rms_user/src/constance/detailReport/index.ts
+++ rms_user/src/constance/detailReport/index.ts
@@ -3,3 +3,5 @@ export const TEAM_PROJECT_REPORT = '팀 프로젝트 보고서';
 export const SUBMITTER = '제출자';
 export const TEACHER = '담당교사 확인';
 export const SCHOOL = '대덕소프트웨어마이스터고등학교';
+export const SUBTITLE = '보고서 작성 요령';
+export const DOWNLOAD = '다운로드';
