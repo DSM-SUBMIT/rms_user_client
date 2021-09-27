4715111a5379ca59b25c6939fd4b46f86ffc5891
kky04994 <kkyo8747@naver.com>
2021-09-28 00:19:12 +0900

    ⚡️ :: (#7) ProjectCreateContainer 생성

--- rms_user/src/route/MainRouter.tsx
+++ rms_user/src/route/MainRouter.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { LoginContainer } from '../containers';
+import { LoginContainer, ProjectCreateContainer } from '../containers';
 
 const MainRouter = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path='/login' component={LoginContainer} />
+        <Route exact path='/ProjectCreateContainer' component={ProjectCreateContainer} />
       </Switch>
     </BrowserRouter>
   );
