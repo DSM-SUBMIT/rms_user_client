c285d2c93456296e47872ae627694c42ed094e30
hyejun <hyejun0608@naver.com>
2021-09-28 20:01:22 +0900

    ⚡️ :: (#9) main redux reducer

--- rms_user/src/modules/redux/reducer/index.ts
+++ rms_user/src/modules/redux/reducer/index.ts
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
+import mainReducer from './main';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({ main: mainReducer });
 
 export type reducerType = ReturnType<typeof rootReducer>;
 export default rootReducer;
