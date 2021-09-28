eeeeb44ccc8ee72f62bbbee7fd737b1503ffc313
hyejun <hyejun0608@naver.com>
2021-09-28 21:58:48 +0900

    ⚡️ :: (#9) 대충 쿼리 파라미터

--- rms_user/src/util/api/main/index.ts
+++ rms_user/src/util/api/main/index.ts
@@ -4,7 +4,7 @@ import { getRequestWithAccessToken } from '../default';
 export const getMain = async (access_token: string) => {
   try {
     const request = getRequestWithAccessToken(access_token);
-    return await request.get(uri.main);
+    return await request.get(uri.main + '?page=1' + '&size=5');
   } catch (error) {
     throw error;
   }
