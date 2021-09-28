e183623a50e8274e951b3f875605728acff38733
hyejun <hyejun0608@naver.com>
2021-09-28 21:54:47 +0900

    ⚡️ :: (#9) redux 상태 props로 전달

--- rms_user/src/containers/main/MainContainer.tsx
+++ rms_user/src/containers/main/MainContainer.tsx
@@ -1,10 +1,20 @@
-import React, { FC, Suspense } from 'react';
+import React, { FC, Suspense, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Main from '../../components/main';
+import { GET_MAIN_FEED } from '../../modules/redux/action/main/interface';
+import useMain from '../../util/hooks/main';
 
 const MainContainer: FC = () => {
+  const { state, setState } = useMain();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type: GET_MAIN_FEED });
+  }, []);
+
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <Main />
+      <Main {...state} {...setState} />
     </Suspense>
   );
 };
