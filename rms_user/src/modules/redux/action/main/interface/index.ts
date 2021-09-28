a6e45f292e1b1da387765fc9799e3ca79299981b
hyejun <hyejun0608@naver.com>
2021-09-28 20:00:56 +0900

    ⚡️ :: (#9) main redux action 추가

--- rms_user/src/modules/redux/action/main/interface/index.ts
+++ rms_user/src/modules/redux/action/main/interface/index.ts
@@ -1,5 +1,6 @@
 export const GET_MAIN_FEED = 'MAIN/GET_MAIN_FEED' as const;
-export const GET_MAIN_FEED_SUCCESS = 'MAIN/GET_MAIN_FEED' as const;
-export const GET_MAIN_FEED_FAILURE = 'MAIN?GET_MAIN_FEED_FAILURE' as const;
+export const GET_MAIN_FEED_SUCCESS = 'MAIN/GET_MAIN_FEED_SUCCESS' as const;
+export const GET_MAIN_FEED_FAILURE = 'MAIN/GET_MAIN_FEED_FAILURE' as const;
 
 export const FIELD = 'MAIN/FIELD' as const;
+export const PAGE = 'MAIN/PAGE' as const;
