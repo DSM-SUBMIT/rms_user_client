5562245033ae21bca95a5b7b8c2454679ddb416e
hyejun <hyejun0608@naver.com>
2021-09-30 18:52:45 +0900

    ⚡️ :: (#27) useLogin hook

---
+++ rms_user/src/util/hooks/login/useLogin.tsx
@@ -0,0 +1,17 @@
+import { useDispatch } from 'react-redux';
+import { useSelectState } from '../default';
+import { setCode } from '../../../modules/redux/action/login';
+
+const useAuth = () => {
+  const dispatch = useDispatch();
+  const state = useSelectState().login;
+  const setState = {
+    setCode: (payload: string) => dispatch(setCode(payload)),
+  };
+  return {
+    state,
+    setState,
+  };
+};
+
+export default useAuth;
