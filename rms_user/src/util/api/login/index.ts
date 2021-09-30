4d77da88d2d47f9104b22ddd125c0de3c6693fb0
hyejun <hyejun0608@naver.com>
2021-09-30 20:14:51 +0900

    ⚡️ :: (#27) 서버 통신

--- rms_user/src/util/api/login/index.ts
+++ rms_user/src/util/api/login/index.ts
@@ -1,10 +1,12 @@
 import uri from '../../../constance/uri';
 import { getRequest } from '../default';
 
-export const getToken = async () => {
+export const getToken = async (tokenRequest: string) => {
   try {
     const request = getRequest();
-    return await request.get(uri.token);
+    const response = await request.post(uri.token + '?code=' + tokenRequest);
+    localStorage.setItem('access_token', response.data.access_token);
+    localStorage.setItem('refresh_token', response.data.refresh_token);
   } catch (error) {
     throw error;
   }
