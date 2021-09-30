e5f2654bdc794179035b55ec3064a7466b0250d8
hyejun <hyejun0608@naver.com>
2021-09-30 20:15:59 +0900

    ⚡️ :: (#27) saga 코드 수정

--- rms_user/src/modules/redux/middleware/login/index.ts
+++ rms_user/src/modules/redux/middleware/login/index.ts
@@ -1,12 +1,16 @@
 import { getToken } from '../../../../util/api/login';
 import { TOKEN } from '../../action/login/interface';
-import { call, put, takeLatest } from 'redux-saga/effects';
+import { call, put, select, takeLatest } from 'redux-saga/effects';
+import { reducerType } from '../../reducer';
+
+const getStateFunc = (state: reducerType): string => state.login.code;
 
 const getTokenSaga = function* (): any {
   const SUCCESS = 'LOGIN/TOKEN_SUCCESS';
   const FAILURE = 'LOGIN/TOKEN_FAILURE';
+  const state = yield select(getStateFunc);
   try {
-    const response = yield call(getToken);
+    const response = yield call(getToken, state);
     yield put({
       type: SUCCESS,
       payload: response ? response.data : null,
