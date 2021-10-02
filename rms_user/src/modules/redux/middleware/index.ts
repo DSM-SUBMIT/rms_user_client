6baacb2997fd8598d2e38f67db9bacaf8eca32ef
hyejun <hyejun0608@naver.com>
2021-10-03 01:58:06 +0900

    ⚡️ :: (#32) detail report redux saga

--- rms_user/src/modules/redux/middleware/index.ts
+++ rms_user/src/modules/redux/middleware/index.ts
@@ -1,6 +1,7 @@
 import { all } from 'redux-saga/effects';
 import mainSaga from './main';
+import detailReportSaga from './detailReport';
 
 export default function* rootSaga() {
-  yield all([mainSaga()]);
+  yield all([mainSaga(), detailReportSaga()]);
 }
