5cc6eef1e675a03c2bd1adb9c0b7d4f0abf81e04
hyejun <hyejun0608@naver.com>
2021-10-03 02:14:45 +0900

    ⚡️ :: (28) page를 매개변수로 받음

--- rms_user/src/util/api/main/index.ts
+++ rms_user/src/util/api/main/index.ts
@@ -1,10 +1,10 @@
 import uri from '../../../constance/uri';
 import { getRequestWithAccessToken } from '../default';
 
-export const getMain = async (access_token: string) => {
+export const getMain = async (access_token: string, page: string) => {
   try {
     const request = getRequestWithAccessToken(access_token);
-    return await request.get(uri.main + '?page=1' + '&size=5');
+    return await request.get(uri.main + '?page=' + page + '&size=5');
   } catch (error) {
     throw error;
   }
