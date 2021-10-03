7cbc9d3d2518e2220e71955b93c1ec1e40df1417
kky04994 <kkyo8747@naver.com>
2021-10-04 03:31:27 +0900

    ⚡️ :: (#30) mypage saga

---
+++ rms_user/src/modules/redux/middleware/mypage/index.ts
@@ -0,0 +1,38 @@
+import { getMypage } from '../../../../util/api/mypage';
+import { call, put, takeLatest } from 'redux-saga/effects';
+import { GET_MYPAGE_FEED } from '../../action/mypage/interface';
+
+const mypageGetSaga = function* (): any {
+  const type = 'GET_MYPAGE_FEED';
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
+  const accessToken = localStorage.getItem('access_token') || '';
+  try {
+    const response = yield call(getMypage, accessToken);
+    yield put({
+      type: SUCCESS,
+      payload: response ? response.data : null,
+    });
+  } catch (error: any) {
+    if (error.response?.data) {
+      yield put({
+        type: FAILURE,
+        payload: { ...error.respons.data, type: type },
+      });
+    } else {
+      yield put({
+        type: FAILURE,
+        payload: {
+          message: 'Network Error',
+          status: 500,
+        },
+      });
+    }
+  }
+};
+
+function* mypageSaga() {
+  yield takeLatest(GET_MYPAGE_FEED, mypageGetSaga);
+}
+
+export default mypageSaga;
