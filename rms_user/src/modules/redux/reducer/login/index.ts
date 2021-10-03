766c680991770197256f83e1f8ef534ffac39a35
hyejun <hyejun0608@naver.com>
2021-10-03 21:56:04 +0900

    ⚡️ :: (#27) state 추가

--- rms_user/src/modules/redux/reducer/login/index.ts
+++ rms_user/src/modules/redux/reducer/login/index.ts
@@ -1,10 +1,11 @@
 import { loginActionType } from '../../action/login';
-import { CODE, TOKEN_FAILURE } from '../../action/login/interface';
+import { CODE, TOKEN, TOKEN_FAILURE, TOKEN_SUCCESS } from '../../action/login/interface';
 import LoginState from './interface';
 
 const initState: LoginState = {
   code: '',
   error: null,
+  isSuccessGetToken: undefined,
 };
 
 const loginReducer = (state: LoginState = initState, action: loginActionType): LoginState => {
@@ -14,10 +15,21 @@ const loginReducer = (state: LoginState = initState, action: loginActionType): L
         ...state,
         code: action.payload,
       };
+    case TOKEN:
+      return {
+        ...state,
+        isSuccessGetToken: undefined,
+      };
+    case TOKEN_SUCCESS:
+      return {
+        ...state,
+        isSuccessGetToken: true,
+      };
     case TOKEN_FAILURE:
       return {
         ...state,
         error: action.payload,
+        isSuccessGetToken: false,
       };
     default:
       return state;
--- rms_user/src/modules/redux/reducer/login/interface/index.ts
+++ rms_user/src/modules/redux/reducer/login/interface/index.ts
@@ -3,6 +3,7 @@ import { error } from '../../../../../models/error';
 interface LoginState {
   code: string;
   error: error | null;
+  isSuccessGetToken: boolean | undefined;
 }
 
 export default LoginState;
