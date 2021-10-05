6bf892ba19ff301b564d85cc712842b71607cd04
hyejun <hyejun0608@naver.com>
2021-10-05 18:31:54 +0900

    ⚡️ :: (#28) 프로젝트를 클릭하였을 때 해당 프로젝트의 id가 저장되고, 프로젝트 상세보기 모달이 열림

--- rms_user/src/components/main/Main.tsx
+++ rms_user/src/components/main/Main.tsx
@@ -1,10 +1,11 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo, useState } from 'react';
 import * as S from './style';
 import Header from '../header';
 import ChooseField from './ChooseField';
 import Project from './Project';
 import { CheckStateType, ProjectListType } from '../../constance/main';
 import ReactPaginate from 'react-paginate';
+import ProjectView from '../modal/view';
 
 interface Props {
   currentPage: number;
@@ -13,49 +14,69 @@ interface Props {
   field: CheckStateType;
   setField: (payload: CheckStateType) => void;
   setPage: (payload: number) => void;
+  setCurrentProjectId: (payload: number) => void;
 }
 
 const Main: FC<Props> = props => {
-  const { currentPage, projectList, totalPages, field, setField, setPage } = props;
+  const {
+    currentPage,
+    projectList,
+    totalPages,
+    field,
+    setField,
+    setPage,
+    setCurrentProjectId,
+  } = props;
+  const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
 
   const pageBtnClickHandler = (selectedItem: { selected: number }) => {
     setPage(selectedItem.selected + 1);
   };
 
+  const projectViewModal = useMemo(() => {
+    if (isOpenModal) return <ProjectView setIsOpenModal={setIsOpenModal} />;
+  }, [isOpenModal]);
+
   return (
-    <S.Main>
-      <Header />
-      <div>
-        <ChooseField field={field} setField={setField} />
+    <>
+      {projectViewModal}
+      <S.Main>
+        <Header />
         <div>
-          {projectList &&
-            projectList.map(data => {
-              return (
-                <Project
-                  projectName={data.projectName}
-                  projectType={data.projectType}
-                  teamName={data.teamName}
-                  fieldList={data.fieldList}
-                  key={data.id}
-                />
-              );
-            })}
-          <ReactPaginate
-            pageCount={totalPages}
-            pageRangeDisplayed={4}
-            marginPagesDisplayed={0}
-            breakLabel={''}
-            previousLabel={'<'}
-            nextLabel={'>'}
-            onPageChange={pageBtnClickHandler}
-            containerClassName={'pagination'}
-            activeClassName={'currentPage'}
-            previousClassName={'pageLabelBtn'}
-            nextClassName={'pageLabelBtn'}
-          />
+          <ChooseField field={field} setField={setField} />
+          <div>
+            {projectList &&
+              projectList.map(data => {
+                return (
+                  <Project
+                    id={data.id}
+                    projectName={data.projectName}
+                    projectType={data.projectType}
+                    teamName={data.teamName}
+                    fieldList={data.fieldList}
+                    key={data.id}
+                    setCurrentProjectId={setCurrentProjectId}
+                    setIsOpenModal={setIsOpenModal}
+                  />
+                );
+              })}
+            <ReactPaginate
+              pageCount={totalPages}
+              pageRangeDisplayed={4}
+              marginPagesDisplayed={0}
+              breakLabel={''}
+              previousLabel={'<'}
+              nextLabel={'>'}
+              onPageChange={pageBtnClickHandler}
+              containerClassName={'pagination'}
+              activeClassName={'currentPage'}
+              previousClassName={'pageLabelBtn'}
+              nextClassName={'pageLabelBtn'}
+            />
+          </div>
         </div>
-      </div>
-    </S.Main>
+      </S.Main>
+    </>
   );
 };
 
