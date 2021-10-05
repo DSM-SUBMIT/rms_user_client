abfe3367080e20bd156ae4cf585f6cb3a2a3cd3c
hyejun <hyejun0608@naver.com>
2021-10-05 19:00:12 +0900

    ⚡️ :: (#32) 로그인 시 이름 저장

--- rms_user/src/modules/redux/action/login/index.ts
+++ rms_user/src/modules/redux/action/login/index.ts
@@ -1,10 +1,11 @@
 import { CODE, TOKEN, TOKEN_FAILURE, TOKEN_SUCCESS } from './interface';
 import { error } from '../../../../models/error';
 import { createAction } from 'typesafe-actions';
+import { loginResponseType } from '../../../../constance/login';
 
 export const setCode = createAction(CODE)<string>();
 export const getToken = createAction(TOKEN)();
-export const getTokenSuccess = createAction(TOKEN_SUCCESS)();
+export const getTokenSuccess = createAction(TOKEN_SUCCESS)<loginResponseType>();
 export const getTokenFailure = createAction(TOKEN_FAILURE)<error>();
 
 export type loginActionType =
