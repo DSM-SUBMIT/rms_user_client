f8460d1b01d08eacb63aa1f9999824a1f381e26e
Kimjimin0807 <jm004471@naver.com>
2021-10-06 01:53:08 +0900

    ⚡️ :: (#36) detailPlanContainer 추가

--- rms_user/src/containers/detailPlan/DetailPlanContainer.tsx
+++ rms_user/src/containers/detailPlan/DetailPlanContainer.tsx
@@ -1,10 +1,30 @@
-import React, { FC, Suspense } from 'react';
+import React, { FC, Suspense, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import DetailPlan from '../../components/detailPlan/DetailPlan';
+import { GET_DETAIL_PLAN } from '../../modules/redux/action/detailPlan/interface';
+import UseDetailPlan from '../../util/hooks/detailPlan';
+
+type DetailPlanParams = {
+  id: string;
+};
 
 const DetailPlanContainer: FC = () => {
+  const { state, setState } = UseDetailPlan();
+  const { id } = useParams<DetailPlanParams>();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setState.setId(id);
+  }, [id]);
+
+  useEffect(() => {
+    dispatch({type: GET_DETAIL_PLAN});
+  }, [state.id]);
+
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <DetailPlan />
+      <DetailPlan {...state}/>
     </Suspense>
   );
 };
