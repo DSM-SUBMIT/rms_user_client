020dd31408d2ff0f45f3bfc32d8e7f46c028d344
hyejun <hyejun0608@naver.com>
2021-10-05 18:31:32 +0900

    ⚡️ :: (#28) 프로젝트를 클릭 하였을 때 해당 프로젝트 상세보기 모달이 열림

--- rms_user/src/components/main/Project.tsx
+++ rms_user/src/components/main/Project.tsx
@@ -2,15 +2,33 @@ import React, { FC } from 'react';
 import * as S from './style';
 
 interface Props {
+  id: number;
   projectName: string;
   projectType: string;
   teamName: string;
   fieldList: Array<string>;
+  setCurrentProjectId: (payload: number) => void;
+  setIsOpenModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const ProjectList: FC<Props> = ({ projectName, projectType, teamName, fieldList }) => {
+const ProjectList: FC<Props> = props => {
+  const {
+    id,
+    projectName,
+    projectType,
+    teamName,
+    fieldList,
+    setCurrentProjectId,
+    setIsOpenModal,
+  } = props;
+
+  const projectClickHandler = (event: React.MouseEvent<HTMLElement>) => {
+    setCurrentProjectId(Number(event.currentTarget.dataset.id));
+    setIsOpenModal(true);
+  };
+
   return (
-    <S.Project>
+    <S.Project onClick={projectClickHandler} data-id={id}>
       <S.ProjectClassification>[{projectType}]</S.ProjectClassification>
       <S.ProjectTitle>{projectName}</S.ProjectTitle>
       <S.ProjectWriter>{teamName}</S.ProjectWriter>
