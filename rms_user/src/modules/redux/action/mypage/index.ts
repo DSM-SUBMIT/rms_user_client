7ad7492281b002df223ea400438dafe5e1b46059
kky04994 <kkyo8747@naver.com>
2021-10-06 00:46:17 +0900

    ⚡️ :: (#30) action name 수정으로 인한 변경

--- rms_user/src/modules/redux/action/mypage/index.ts
+++ rms_user/src/modules/redux/action/mypage/index.ts
@@ -1,16 +1,16 @@
 import { createAction } from 'typesafe-actions';
 import { error } from '../../../../models/error';
-import { CheckStateType } from '../../../../constance/mypage';
+import { CheckStateType, MypageResponseType } from '../../../../constance/mypage';
 import {
   GET_MYPAGE_FEED,
   GET_MYPAGE_FEED_SUCCESS,
-  GET_MYPAGE_FEED_FALURE,
+  GET_MYPAGE_FEED_FAILURE,
   FIELD,
 } from './interface';
 
 export const getMypage = createAction(GET_MYPAGE_FEED)();
-export const getMypageSuccess = createAction(GET_MYPAGE_FEED_SUCCESS)();
-export const getMypageFaillure = createAction(GET_MYPAGE_FEED_FALURE)<error>();
+export const getMypageSuccess = createAction(GET_MYPAGE_FEED_SUCCESS)<MypageResponseType>();
+export const getMypageFaillure = createAction(GET_MYPAGE_FEED_FAILURE)<error>();
 export const setField = createAction(FIELD)<CheckStateType>();
 
 export type mypageActionType =
