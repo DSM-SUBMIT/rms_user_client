4944b609ba311b0f6a6c622efef8bf578915f5a6
Kimjimin0807 <jm004471@naver.com>
2021-10-06 01:52:29 +0900

    ⚡️ :: (#36) detail plan saga 추가

---
+++ rms_user/src/modules/redux/middleware/detailPlan/index.ts
@@ -0,0 +1,43 @@
+import { call, put, select, takeLatest } from '@redux-saga/core/effects';
+import { getDetailPlan } from '../../../../util/api/detailPlan';
+import { reducerType } from '../../reducer';
+import DetailPlanState from '../../reducer/detailPlan/interface';
+import {GET_DETAIL_PLAN} from '../../action/detailPlan/interface';
+
+const getStateFunc = (state: reducerType): DetailPlanState => state.detailPlan;
+
+const getDetailPlanSaga = function* (): any {
+  const type = 'PLAN/GET_DETAIL_PLAN';
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
+  const state = yield select(getStateFunc);
+  const accessToken = localStorage.getItem('access_token') || '';
+  try {
+    const response = yield call(getDetailPlan, accessToken, state.id);
+    yield put({
+      type: SUCCESS,
+      payload: response ? response.data : null,
+    });
+  } catch (error: any) {
+    if (error.response?.data) {
+      yield put({
+        type: FAILURE,
+        payload: { ...error.response.data, type: type },
+      });
+    } else {
+      yield put({
+        type: FAILURE,
+        payload: {
+          message: 'Network Error',
+          status: 500,
+        },
+      });
+    }
+  }
+};
+
+function* detailPlanSaga(){
+  yield takeLatest(GET_DETAIL_PLAN, getDetailPlanSaga);
+}
+
+export default detailPlanSaga;
--- rms_user/src/modules/redux/middleware/index.ts
+++ rms_user/src/modules/redux/middleware/index.ts
@@ -1,6 +1,7 @@
 import { all } from 'redux-saga/effects';
+import detailPlanSaga from './detailPlan';
 import loginSaga from './login';
 
 export default function* rootSaga() {
-  yield all([loginSaga()]);
+  yield all([loginSaga(), detailPlanSaga()]);
 }
