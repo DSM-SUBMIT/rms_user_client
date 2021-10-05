83e3f44cbf14ee7675c0730bec46ae9c9cd84abc
kky04994 <kkyo8747@naver.com>
2021-10-06 00:51:50 +0900

    ⚡️ :: (#30) Field add

--- rms_user/src/util/api/mypage/index.ts
+++ rms_user/src/util/api/mypage/index.ts
@@ -1,11 +1,24 @@
+import { CheckStateType } from '../../../constance/mypage';
 import uri from '../../../constance/uri';
 import { getRequestWithAccessToken } from '../default';
 
-export const getMypage = async (access_token: string) => {
+export const getMypage = async (access_token: string, field: CheckStateType) => {
   try {
     const request = getRequestWithAccessToken(access_token);
-    return await request.get(uri.mypage);
+    const response = await request.get(uri.mypage);
+    return response;
   } catch (error) {
     throw error;
   }
+};
+
+export const SetField = (field: CheckStateType) => {
+  let fieldList: Array<string> = [];
+  if (field.web === true) fieldList.push('WEB');
+  if (field.app === true) fieldList.push('APP');
+  if (field.game === true) fieldList.push('GAME');
+  if (field.embedded === true) fieldList.push('EMBEDDED');
+  if (field.aiAndBigData === true) fieldList.push('AI_BIGDATA');
+  if (field.security === true) fieldList.push('SECURITY');
+  return fieldList.join(',');
 };
