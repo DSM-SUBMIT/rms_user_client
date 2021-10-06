3c371f7dfa6b07339367f8451ddb41c2ddf84145
Kimjimin0807 <jm004471@naver.com>
2021-10-06 23:02:38 +0900

    ⚡️ :: (#36) 계획서 상세보기 에러 처리

--- rms_user/src/containers/detailPlan/DetailPlanContainer.tsx
+++ rms_user/src/containers/detailPlan/DetailPlanContainer.tsx
@@ -1,6 +1,6 @@
 import React, { FC, Suspense, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import DetailPlan from '../../components/detailPlan/DetailPlan';
 import { GET_DETAIL_PLAN } from '../../modules/redux/action/detailPlan/interface';
 import UseDetailPlan from '../../util/hooks/detailPlan';
@@ -12,19 +12,33 @@ type DetailPlanParams = {
 const DetailPlanContainer: FC = () => {
   const { state, setState } = UseDetailPlan();
   const { id } = useParams<DetailPlanParams>();
+  const history = useHistory();
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (state.error?.status === 401) {
+      alert('로그인 후 이용해주세요.');
+      history.push('/');
+    } else if (state.error?.status === 403) {
+      alert('권한이 없습니다.');
+      history.goBack();
+    } else if (state.error?.status === 404) {
+      alert('존재하지 않는 계획서 입니다.');
+      history.goBack();
+    }
+  }, [state.error]);
 
   useEffect(() => {
     setState.setId(id);
   }, [id]);
 
   useEffect(() => {
-    dispatch({type: GET_DETAIL_PLAN});
+    dispatch({ type: GET_DETAIL_PLAN });
   }, [state.id]);
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <DetailPlan {...state}/>
+      <DetailPlan {...state} />
     </Suspense>
   );
 };
