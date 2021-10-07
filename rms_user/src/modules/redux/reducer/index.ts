964636c9bdae39babbec77d329192c44f897e96e
hyejun <hyejun0608@naver.com>
2021-10-07 22:22:43 +0900

    ⚡️ :: (#27)  info redux reducer

--- rms_user/src/modules/redux/reducer/index.ts
+++ rms_user/src/modules/redux/reducer/index.ts
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import headerReducer from './header';
 import loginReducer from './login';
 import mainReducer from './main';
+import infoReducer from './info';
 
 const rootReducer = combineReducers({
   main: mainReducer,
   login: loginReducer,
   header: headerReducer,
+  info: infoReducer,
 });
 
 export type reducerType = ReturnType<typeof rootReducer>;
