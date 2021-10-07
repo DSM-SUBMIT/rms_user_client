76373f1c3568c52dc8e7823d097137203e28f1d0
hyejun <hyejun0608@naver.com>
2021-10-07 10:37:35 +0900

    ⚡️ :: (#42) header redux middleware header

---
+++ rms_user/src/modules/redux/middleware/header/index.ts
@@ -0,0 +1,38 @@
+import { getName } from '../../../../util/api/header';
+import { call, put, takeLatest } from 'redux-saga/effects';
+import { GET_NAME } from '../../action/header/interface';
+
+const nameGetSaga = function* (): any {
+  const type = 'HEADER/GET_NAME';
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
+  const accessToken = localStorage.getItem('access_token') || '';
+  try {
+    const response = yield call(getName, accessToken);
+    yield put({
+      type: SUCCESS,
+      payload: response ? response.data : null,
+    });
+  } catch (error: any) {
+    if (error.response?.data) {
+      yield put({
+        type: FAILURE,
+        payload: { ...error.response.data, type: type },
+      });
+    } else {
+      yield put({
+        type: FAILURE,
+        payload: {
+          message: 'Network Error',
+          status: 500,
+        },
+      });
+    }
+  }
+};
+
+function* headerSaga() {
+  yield takeLatest(GET_NAME, nameGetSaga);
+}
+
+export default headerSaga;
