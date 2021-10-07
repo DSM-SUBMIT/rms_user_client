9cff9198e4924161bfac1795bf6827c1acc2129b
hyejun <hyejun0608@naver.com>
2021-10-07 10:36:30 +0900

    ⚡️ :: (#42) root reducer에 hedaer reducer 추가

--- rms_user/src/modules/redux/reducer/index.ts
+++ rms_user/src/modules/redux/reducer/index.ts
@@ -1,8 +1,13 @@
 import { combineReducers } from 'redux';
+import headerReducer from './header';
 import loginReducer from './login';
 import mainReducer from './main';
 
-const rootReducer = combineReducers({ main: mainReducer, login: loginReducer });
+const rootReducer = combineReducers({
+  main: mainReducer,
+  login: loginReducer,
+  header: headerReducer,
+});
 
 export type reducerType = ReturnType<typeof rootReducer>;
 export default rootReducer;
