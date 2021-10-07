b3eceb095e0b09ca54a2b15211415e4d3a509820
hyejun <hyejun0608@naver.com>
2021-10-07 19:40:35 +0900

    ⚡️ :: (#27)  info constance

---
+++ rms_user/src/constance/info/index.ts
@@ -0,0 +1,8 @@
+export const TITLE = '정보 입력';
+export const EXPLAIN = '학년, 반, 번호를 입력해 주세요.';
+export const INFOBTN = '확인';
+export const INPUT = [
+  { id: 'grade', text: '학년' },
+  { id: 'class', text: '반' },
+  { id: 'number', text: '번호' },
+];
