025087cbaf5a3d627275faacad6cf8a96ddf896d
kky04994 <kkyo8747@naver.com>
2021-10-10 18:54:58 +0900

    ⚡️ :: (#30) setState 추가

--- rms_user/src/containers/mypage/MyPageContainer.tsx
+++ rms_user/src/containers/mypage/MyPageContainer.tsx
@@ -5,14 +5,14 @@ import { GET_MYPAGE_FEED } from '../../modules/redux/action/mypage/interface';
 import useMypage from '../../util/hooks/mypage';
 
 const MyPageContainer: FC = () => {
-  const { state } = useMypage();
+  const { state, setState } = useMypage();
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch({ type: GET_MYPAGE_FEED });
   }, []);
   return (
     <Suspense fallback={<div>Loading</div>}>
-      <MyPage {...state} />
+      <MyPage {...state} {...setState} />
     </Suspense>
   );
 };
--- rms_user/src/util/hooks/mypage/useMypage.tsx
+++ rms_user/src/util/hooks/mypage/useMypage.tsx
@@ -1,8 +1,22 @@
 import { useSelectState } from '../default';
+import { useDispatch } from 'react-redux';
+import { setModalOff, setModalOn, setType } from '../../../modules/redux/action/modal';
 
 const useMypage = () => {
   const state = useSelectState().mypage;
-  return { state };
+  const dispatch = useDispatch();
+  const setState = {
+    setModalOff: (payload: string) => {
+      dispatch(setModalOff(payload));
+    },
+    setModalOn: (payload: string) => {
+      dispatch(setModalOn(payload));
+    },
+    setType: (payload: string) => {
+      dispatch(setType(payload));
+    },
+  };
+  return { state, setState };
 };
 
 export default useMypage;
