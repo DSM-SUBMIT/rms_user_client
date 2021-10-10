daf8e9823a0c6fc0aa27cf00d906a79c8071e9f7
kky04994 <kkyo8747@naver.com>
2021-10-10 23:53:27 +0900

    :coffin: :: (#38) 불필요한 파일 삭제

--- rms_user/src/constance/project/index.ts
+++
@@ -1,6 +0,0 @@
-export const TechStatck = '사용한 기술 스택';
-export const Plan = '계획서';
-export const Report = '보고서';
-export const GitHub = 'Github';
-export const Api = 'API 서비스';
-export const Details = '기능 명세';
