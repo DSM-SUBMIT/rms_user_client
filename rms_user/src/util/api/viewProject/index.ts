c3c2558e80930ff326b476caf5146569a7b8d171
kky04994 <kkyo8747@naver.com>
2021-10-11 19:01:15 +0900

    ⚡️ :: (#38) viewProject api

---
+++ rms_user/src/util/api/viewProject/index.ts
@@ -0,0 +1,12 @@
+import uri from '../../../constance/uri';
+import { getRequestWithAccessToken } from '../default';
+
+export const getProjectView = async (access_token: string, id: string) => {
+  try {
+    const request = getRequestWithAccessToken(access_token);
+    const response = await request.get(uri.project + `/${id}`);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
