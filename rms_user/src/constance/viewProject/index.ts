77faefa887fafbca4e51c3c1687a7edcc3ad19c8
kky04994 <kkyo8747@naver.com>
2021-10-11 18:58:08 +0900

    ⚡️ :: (#38) constance에 viewProject 추가

---
+++ rms_user/src/constance/viewProject/index.ts
@@ -0,0 +1,25 @@
+export const TechStatck = '사용한 기술 스택';
+export const Plan = '계획서';
+export const Report = '보고서';
+export const GitHub = 'Github';
+export const Api = 'API 서비스';
+export const Details = '기능 명세';
+
+export type MemberListType = {
+  email: string;
+  name: string;
+  role: string;
+};
+
+export type ViewProjectType = {
+  projectName: string;
+  projectType: string;
+  serviceUrl: string;
+  teamName: string;
+  techStack: string;
+  memberList: [];
+  fileList: [];
+  docsUrl: string;
+  githubUrl: string;
+  id: number;
+};
