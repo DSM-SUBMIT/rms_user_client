eff78236941d44e0cb28e4659a4b576109989228
kky04994 <kkyo8747@naver.com>
2021-10-12 23:46:07 +0900

    ⚡️ :: (#38) reducer viewProject 생성

---
+++ rms_user/src/modules/redux/reducer/viewProject/index.ts
@@ -0,0 +1,48 @@
+import { viewProjectActionType } from '../../action/viewProject';
+import {
+  GET_PROJECT_CONTENTS_SUCCESS,
+  GET_PROJECT_CONTENTS_FAILURE,
+} from '../../action/viewProject/interface';
+import ViewProjectState from './interface';
+
+const initState: ViewProjectState = {
+  projectName: '',
+  projectType: '',
+  serviceUrl: '',
+  teamName: '',
+  techStack: '',
+  memberList: [],
+  fieledList: [],
+  id: 1,
+  docsUrl: '',
+  githubUrl: '',
+  error: null,
+};
+
+const viewProjectReducer = (state: ViewProjectState = initState, action: viewProjectActionType) => {
+  switch (action.type) {
+    case GET_PROJECT_CONTENTS_SUCCESS:
+      return {
+        ...state,
+        projectName: action.payload.projectName,
+        projectType: action.payload.projectType,
+        serviceUrl: action.payload.serviceUrl,
+        teamName: action.payload.teamName,
+        techStack: action.payload.techStack,
+        memberList: action.payload.memberList,
+        fileList: action.payload.fileList,
+        docsUrl: action.payload.docsUrl,
+        githubUrl: action.payload.githubUrl,
+      };
+
+    case GET_PROJECT_CONTENTS_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default viewProjectReducer;
---
+++ rms_user/src/modules/redux/reducer/viewProject/interface/index.ts
@@ -0,0 +1,17 @@
+import { error } from '../../../../../models/error';
+import { MemberListType } from '../../../../../constance/viewProject';
+interface ViewProjectState {
+  projectName: string;
+  projectType: string;
+  serviceUrl: string;
+  teamName: string;
+  techStack: string;
+  memberList: Array<MemberListType>;
+  fieledList: Array<string>;
+  id: number;
+  docsUrl: string;
+  githubUrl: string;
+  error: error | null;
+}
+
+export default ViewProjectState;
