8bef635dfa6b3ef9c1a7fdef9d5c3998f6d3e8ca
kky04994 <kkyo8747@naver.com>
2021-10-18 21:49:28 +0900

    ⚡️ :: (#38) 연동 시 오류 코드 수정

--- rms_user/src/modules/redux/middleware/viewProject/index.ts
+++ rms_user/src/modules/redux/middleware/viewProject/index.ts
@@ -3,20 +3,25 @@ import { reducerType } from '../../reducer';
 import ViewProjectState from '../../reducer/viewProject/interface';
 import { GET_PROJECT_CONTENTS } from '../../action/viewProject/interface';
 import { getProjectView } from '../../../../util/api/viewProject';
+import MainState from '../../reducer/main/interface';
 const getStateFunc = (state: reducerType): ViewProjectState => state.viewProject;
+const getMainStateFunc = (state: reducerType): MainState => state.main;
 
 const viewProjectGetSaga = function* (): any {
-  const type = 'VIEWPROJECT/GET_PROJECT_CONTENTS';
+  const type = 'PROJECT/GET_PROJECT_CONTENTS';
   const SUCCESS = `${type}_SUCCESS`;
   const FAILURE = `${type}_FAILURE`;
   const state = yield select(getStateFunc);
+  const mainState = yield select(getMainStateFunc);
   const accessToken = localStorage.getItem('access_token') || '';
   try {
-    const response = yield call(getProjectView, accessToken, state.id);
+    const response = yield call(getProjectView, accessToken, mainState.currentProjectId);
+
     yield put({
       type: SUCCESS,
       payload: response ? response.data : null,
     });
+    console.log('test', response);
   } catch (error: any) {
     if (error.response?.data) {
       yield put({
--- rms_user/src/util/api/viewProject/index.ts
+++ rms_user/src/util/api/viewProject/index.ts
@@ -4,7 +4,7 @@ import { getRequestWithAccessToken } from '../default';
 export const getProjectView = async (access_token: string, id: string) => {
   try {
     const request = getRequestWithAccessToken(access_token);
-    const response = await request.get(uri.project + `/${id}`);
+    const response = await request.get(uri.projectview + `/${id}`);
     return response;
   } catch (error) {
     throw error;
