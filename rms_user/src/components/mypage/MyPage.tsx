aaa62433017010c9873a4e0bfdecf5a1789414ab
kky04994 <kkyo8747@naver.com>
2021-10-19 16:07:04 +0900

    ⚡️ :: (#38) 기능 연동 후 MyPage 수정

--- rms_user/src/components/mypage/MyPage.tsx
+++ rms_user/src/components/mypage/MyPage.tsx
@@ -4,27 +4,38 @@ import Header from '../header';
 import ProjectContent from './projectcontent';
 import { PROJECT, CREATE_PROJECT, ProjectListType } from '../../constance/mypage';
 import { Plus } from '../../assets';
-import { setModalOn } from '../../modules/redux/action/modal';
 import { useModal } from '../../util/hooks/modal';
+import useViewMyProject from '../../util/hooks/viewMyProject';
+import ProjectView from '../modal/view';
 interface Props {
+  currentPage: number;
   name: string;
   email: string;
   projectList: Array<ProjectListType>;
   studentNumber: number;
   setModalOn: (payload: string) => void;
   setModalOff: (payload: string) => void;
+  setCurrentProjectId: (payload: number) => void;
 }
 
 const MyPage: FC<Props> = props => {
-  const { name, email, projectList, studentNumber, setModalOff } = props;
+  const { state } = useViewMyProject();
+  const { name, email, projectList, studentNumber, setCurrentProjectId, currentPage } = props;
   const { setState } = useModal();
+  const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
 
   const onClickChangePasswordModal = () => {
     setState.setModalOn('modifyNumber');
   };
 
+  const myProjectViewModal = useMemo(() => {
+    if (isOpenModal)
+      return <ProjectView setIsOpenModal={setIsOpenModal} {...state} projectId={currentPage} />;
+  }, [isOpenModal, state, currentPage]);
+
   return (
     <>
+      {myProjectViewModal}
       <Header />
       <S.MyPage>
         <S.Content>
@@ -49,6 +60,9 @@ const MyPage: FC<Props> = props => {
                     projectType={data.projectType}
                     teamName={data.teamName}
                     fieldList={data.fieldList}
+                    setIsOpenModal={setIsOpenModal}
+                    setCurrentProjectId={setCurrentProjectId}
+                    id={data.id}
                   />
                 );
               })}
