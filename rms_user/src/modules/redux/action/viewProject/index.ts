a15220742b285faf3c92f1ac57158ef5c16bc38f
kky04994 <kkyo8747@naver.com>
2021-10-20 12:47:01 +0900

    ⚡️ :: (#38) 불필요한 코드 삭제

--- rms_user/src/modules/redux/action/viewProject/index.ts
+++ rms_user/src/modules/redux/action/viewProject/index.ts
@@ -6,14 +6,6 @@ import {
   GET_MY_PROJECT_CONTENTS,
   GET_MY_PROJECT_CONTENTS_SUCCESS,
   GET_MY_PROJECT_CONTENTS_FAILURE,
-  GET_PLAN_ACCEPTED_SUCCESS,
-  GET_PLAN_ACCEPTED_FAILURE,
-  GET_REPORT_ACCEPTED_SUCCESS,
-  GET_REPORT_ACCEPTED_FAILURE,
-  GET_PLAN_SUBMITTED_SUCCESS,
-  GET_PLAN_SUBMITTED_FAILURE,
-  GET_REPORT_SUBMITTED_SUCCESS,
-  GET_REPORT_SUBMITTED_FAILURE,
 } from './interface';
 import { createAction } from 'typesafe-actions';
 import { ViewMyProjectType, ViewProjectType } from '../../../../constance/viewProject';
@@ -26,14 +18,6 @@ export const getMyProjectViewSuccess = createAction(
   GET_MY_PROJECT_CONTENTS_SUCCESS,
 )<ViewMyProjectType>();
 export const getMyProjectViewFailure = createAction(GET_MY_PROJECT_CONTENTS_FAILURE)<error>();
-// export const getAcceptedPlanSuccess = createAction(GET_PLAN_ACCEPTED_SUCCESS);
-// export const getAcceptedPlanFailure = createAction(GET_PLAN_ACCEPTED_FAILURE)<error>();
-// export const getAcceptedReportSuccess = createAction(GET_REPORT_ACCEPTED_SUCCESS);
-// export const getAcceptedReportFailure = createAction(GET_REPORT_ACCEPTED_FAILURE)<error>();
-// export const getSubmittedPlanSuccess = createAction(GET_PLAN_SUBMITTED_SUCCESS);
-// export const getSubmittedPlanFailure = createAction(GET_PLAN_SUBMITTED_FAILURE)<error>();
-// export const getSubmittedReportSuccess = createAction(GET_REPORT_SUBMITTED_SUCCESS);
-// export const getSubmittedReportFailure = createAction(GET_REPORT_SUBMITTED_FAILURE)<error>();
 
 export type viewProjectActionType =
   | ReturnType<typeof getProjectView>
@@ -41,12 +25,3 @@ export type viewProjectActionType =
   | ReturnType<typeof getProjectViewFailure>
   | ReturnType<typeof getMyProjectView>
   | ReturnType<typeof getMyProjectViewSuccess>;
-// | ReturnType<typeof getMyProjectViewFailure>
-// | ReturnType<typeof getAcceptedPlanSuccess>
-// | ReturnType<typeof getAcceptedPlanFailure>
-// | ReturnType<typeof getAcceptedReportSuccess>
-// | ReturnType<typeof getAcceptedReportFailure>
-// | ReturnType<typeof getSubmittedPlanSuccess>
-// | ReturnType<typeof getSubmittedPlanFailure>
-// | ReturnType<typeof getSubmittedReportSuccess>
-// | ReturnType<typeof getSubmittedReportFailure>;
