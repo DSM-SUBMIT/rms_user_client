05cd4140062b86848587038217a201eea22d25f5
kky04994 <kkyo8747@naver.com>
2021-10-21 23:48:10 +0900

    ⚡️ :: (#39) userList action 추가

--- rms_user/src/modules/redux/action/porject/index.ts
+++ rms_user/src/modules/redux/action/porject/index.ts
@@ -1,7 +1,38 @@
 import { error } from '../../../../models/error';
 import { createAction } from 'typesafe-actions';
-import { CREATE_PROJECT, CREATE_PROJECT_SUCCESS, CREATE_PROJECT_FAILURE } from './interface';
+import {
+  PROJECTNAME,
+  PROJECTTYPE,
+  TEACHER,
+  TEAMNAME,
+  TECHSTACKS,
+  FIELDLIST,
+  MEMBERLIST,
+  USERSLIST,
+  USERSLIST_SUCCESS,
+  USERSLIST_FAILURE,
+} from './interface';
+import { MemberListType, UsersListType } from '../../../../constance/project';
 
-export const setCreateProject = createAction(CREATE_PROJECT);
+export const setProjectName = createAction(PROJECTNAME)<string>();
+export const setProjectType = createAction(PROJECTTYPE)<string>();
+export const setTeacher = createAction(TEACHER)<string>();
+export const setTeamName = createAction(TEAMNAME)<string>();
+export const setTechStacks = createAction(TECHSTACKS)<string>();
+export const setFieldList = createAction(FIELDLIST)();
+export const setMemberList = createAction(MEMBERLIST)<MemberListType>();
+export const getUserList = createAction(USERSLIST)();
+export const getUserListSuccess = createAction(USERSLIST_SUCCESS)<UsersListType>();
+export const getUserListFailure = createAction(USERSLIST_FAILURE)<error>();
 
-export type ProjectActionType = ReturnType<typeof setCreateProject>;
+export type createProjectActionType =
+  | ReturnType<typeof setProjectName>
+  | ReturnType<typeof setProjectType>
+  | ReturnType<typeof setTeacher>
+  | ReturnType<typeof setTeamName>
+  | ReturnType<typeof setTechStacks>
+  | ReturnType<typeof setFieldList>
+  | ReturnType<typeof setMemberList>
+  | ReturnType<typeof getUserList>
+  | ReturnType<typeof getUserListSuccess>
+  | ReturnType<typeof getUserListFailure>;
