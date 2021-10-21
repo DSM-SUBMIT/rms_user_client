4756769e443c00766fc7f82a16b333dcc7730f2e
kky04994 <kkyo8747@naver.com>
2021-10-21 20:13:40 +0900

    ⚡️ :: (#39) x.svg 이미지 추가

--- rms_user/src/assets/index.ts
+++ rms_user/src/assets/index.ts
@@ -2,5 +2,5 @@ export { default as Profile } from './header/Profile.svg';
 export { default as Close } from './project/close.svg';
 export { default as Plus } from './mypage/Plus.svg';
 export { default as Search } from './project/search.svg';
-export { default as Close } from './project/close.svg';
 export { default as Github } from './project/github.svg';
+export { default as X } from './project/x.svg';
