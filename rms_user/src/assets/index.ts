2dc264f38fb83d73b879cc672f31f40ce572f2c5
kky04994 <kkyo8747@naver.com>
2021-10-24 22:59:38 +0900

    ⚡️ :: (#39) svg 추가

--- rms_user/src/assets/index.ts
+++ rms_user/src/assets/index.ts
@@ -4,3 +4,5 @@ export { default as Plus } from './mypage/Plus.svg';
 export { default as Search } from './project/search.svg';
 export { default as Github } from './project/github.svg';
 export { default as X } from './project/x.svg';
+export { default as Arrow } from './project/arrow.svg';
+export { default as ModalClose } from './project/deleteClose.svg';
