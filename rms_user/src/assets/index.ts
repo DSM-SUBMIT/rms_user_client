1a71a7169b190d988fcc4ad0eb9d0f2d6bf31e29
kky04994 <kkyo8747@naver.com>
2021-10-25 10:55:03 +0900

    ⚡️ :: (#39) field svg 추가

--- rms_user/src/assets/index.ts
+++ rms_user/src/assets/index.ts
@@ -6,3 +6,4 @@ export { default as Github } from './project/github.svg';
 export { default as X } from './project/x.svg';
 export { default as Arrow } from './project/arrow.svg';
 export { default as ModalClose } from './project/deleteClose.svg';
+export { default as FieldClose } from './project/fieldClose.svg';
