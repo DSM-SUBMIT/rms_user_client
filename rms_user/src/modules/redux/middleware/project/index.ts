4c5e061a6195291c5d8823f7bef9ea0a4a8f8545
kky04994 <kkyo8747@naver.com>
2021-10-26 20:29:43 +0900

    ⚡️ :: (#39) request 추가

--- rms_user/src/modules/redux/middleware/project/index.ts
+++ rms_user/src/modules/redux/middleware/project/index.ts
@@ -1,8 +1,9 @@
 import { call, put, select, takeLatest } from 'redux-saga/effects';
 import { reducerType } from '../../reducer';
 import { CREATE_PROJECT } from '../../action/porject/interface';
-import { createProject } from '../../../../util/api/project';
+import { createProject, projectCreateStateToRequest } from '../../../../util/api/project';
 import ProjectState from '../../reducer/project/interface';
+import MypageState from '../../reducer/mypage/interface';
 const getStateFunc = (state: reducerType): ProjectState => state.project;
 
 const setProjectSaga = function* (): any {
@@ -10,14 +11,14 @@ const setProjectSaga = function* (): any {
   const SUCCESS = `${type}_SUCCESS`;
   const FAILURE = `${type}_FAILURE`;
   const state = yield select(getStateFunc);
+  const request = projectCreateStateToRequest(state);
   const accessToken = localStorage.getItem('access_token') || '';
   try {
-    const response = yield call(createProject, accessToken);
+    const response = yield call(createProject, accessToken, request);
     yield put({
       type: SUCCESS,
       payload: response ? response.data : null,
     });
-    console.log(response.data);
   } catch (error: any) {
     if (error.response?.data) {
       yield put({
--- rms_user/src/util/api/project/index.ts
+++ rms_user/src/util/api/project/index.ts
@@ -1,10 +1,24 @@
+import { ProjectCreateType } from '../../../constance/project';
 import uri from '../../../constance/uri';
+import { reducerType } from '../../../modules/redux/reducer';
 import { getRequestWithAccessToken } from '../default';
 
-export const createProject = async (access_token: string) => {
+export const projectCreateStateToRequest = (state: reducerType['project']): ProjectCreateType => {
+  return {
+    fieldList: state.fieldList,
+    memberList: state.memberList,
+    projectName: state.projectName,
+    projectType: state.projectType,
+    teacher: state.teacher,
+    teamName: state.teamName,
+    techStacks: state.techStacks,
+  };
+};
+
+export const createProject = async (access_token: string, state: ProjectCreateType) => {
   try {
     const request = getRequestWithAccessToken(access_token);
-    return await request.post(uri.project);
+    return await request.post(uri.project, state);
   } catch (error) {
     throw error;
   }
@@ -33,6 +47,15 @@ export const deleteProject = async (access_token: string, id: string) => {
   try {
     const request = getRequestWithAccessToken(access_token);
     return await request.delete(uri.project + `/${id}`);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const modifyUrl = async (access_token: string, id: string) => {
+  try {
+    const request = getRequestWithAccessToken(access_token);
+    return await request.delete(uri.project + `/${id}` + '/url');
   } catch (error) {
     throw error;
   }
