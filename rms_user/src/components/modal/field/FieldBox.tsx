e8b9ab7ab0b1b31ca0b4efb803c6dc605040e18a
kky04994 <kkyo8747@naver.com>
2021-10-26 19:01:53 +0900

    ⚡️ :: (#39) FieldBox 생성

---
+++ rms_user/src/components/modal/field/FieldBox.tsx
@@ -0,0 +1,47 @@
+import React, { FC, useState } from 'react';
+import * as S from './style';
+import { FieldListSelect } from '../../../constance/project';
+import { setFieldList } from '../../../modules/redux/action/porject';
+import { DataIdType } from '../../../constance/main';
+import { useSelector } from 'react-redux';
+
+interface Props {
+  setIsOpenModal?: React.Dispatch<React.SetStateAction<boolean>>;
+  setFieldList: (payload: string[]) => void;
+  fieldList: string[];
+}
+
+const FieldBox: FC<Props> = props => {
+  const { setFieldList, fieldList } = props;
+
+  const onClickBox = (e: any, content: string) => {
+    const dataId = e.currentTarget.dataset.id;
+    const isClick = e.currentTarget.checked;
+    if ((fieldList.length = 0)) {
+      fieldList.filter((conten, i) => conten !== undefined);
+    }
+
+    if (isClick) setFieldList(fieldList.concat(dataId));
+    else setFieldList(fieldList.filter((item: string) => content !== item));
+  };
+
+  return (
+    <>
+      <S.FieldBox>
+        {FieldListSelect.map((item, index) => (
+          <S.SelectBox>
+            <S.CheckBox
+              type='checkbox'
+              data-id={item.id}
+              key={index}
+              onClick={e => onClickBox(e, item.content)}
+            />
+            <S.FieldName>{item.content}</S.FieldName>
+          </S.SelectBox>
+        ))}
+      </S.FieldBox>
+    </>
+  );
+};
+
+export default FieldBox;
