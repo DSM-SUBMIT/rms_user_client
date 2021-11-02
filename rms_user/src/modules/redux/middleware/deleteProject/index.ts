72d487ba838529e813cd64e2c0bbe0de0658cc29
kky04994 <kkyo8747@naver.com>
2021-11-02 21:43:45 +0900

    ⚡️ :: (#39) deleteProjectSaga 수정

--- rms_user/src/modules/redux/middleware/deleteProject/index.ts
+++ rms_user/src/modules/redux/middleware/deleteProject/index.ts
@@ -1,19 +1,19 @@
 import { call, put, select, takeLatest } from '@redux-saga/core/effects';
-import { getDetailPlan } from '../../../../util/api/detailPlan';
+import { deleteProject } from '../../../../util/api/project';
 import { reducerType } from '../../reducer';
-import DetailPlanState from '../../reducer/detailPlan/interface';
-import { PROJECTDELETE } from '../../action/porject/interface';
+import { DELETE_PROJECT } from '../../action/porject/interface';
+import MypageState from '../../reducer/mypage/interface';
 
-const getStateFunc = (state: reducerType): DetailPlanState => state.detailPlan;
+const getMyPageStateFunc = (state: reducerType): MypageState => state.mypage;
 
 const setDeleteProjectSaga = function* (): any {
-  const type = 'PROJECT/DELETE_PROJECTDELETE';
+  const type = 'PROJECT/DELETE_PROJECT';
   const SUCCESS = `${type}_SUCCESS`;
   const FAILURE = `${type}_FAILURE`;
-  const state = yield select(getStateFunc);
+  const MypageState = yield select(getMyPageStateFunc);
   const accessToken = localStorage.getItem('access_token') || '';
   try {
-    const response = yield call(getDetailPlan, accessToken, state.id);
+    const response = yield call(deleteProject, accessToken, MypageState.currentProjectId);
     yield put({
       type: SUCCESS,
       payload: response ? response.data : null,
@@ -37,7 +37,7 @@ const setDeleteProjectSaga = function* (): any {
 };
 
 function* deleteProjectSaga() {
-  yield takeLatest(PROJECTDELETE, setDeleteProjectSaga);
+  yield takeLatest(DELETE_PROJECT, setDeleteProjectSaga);
 }
 
 export default deleteProjectSaga;
