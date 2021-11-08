3aafdfd72db2bf06fb7a3ffc85225b42c3e494a9
kky04994 <kkyo8747@naver.com>
2021-11-08 23:41:47 +0900

    ⚡️ :: (#39) ModfiyType & UrlType 추가

--- rms_user/src/constance/project/index.ts
+++ rms_user/src/constance/project/index.ts
@@ -6,13 +6,14 @@ export const Api = 'API 서비스';
 export const Details = '기능 명세';
 
 export const ClassificationSelect = [
-  '프로젝트 실무 1',
-  '프로젝트 실무 2',
-  '소프트웨어 공학',
-  '개인 프로젝트',
-  '팀 프로젝트',
-  '동아리 프로젝트',
+  { id: 'CLUB', content: '동아리 프로젝트' },
+  { id: 'PRO1', content: '프로젝트 실무 1' },
+  { id: 'PRO2', content: '프로젝트 실무 2' },
+  { id: 'SOFE', content: '소프트웨어 공학' },
+  { id: 'PERS', content: '개인 프로젝트' },
+  { id: 'TEAM', content: '팀 프로젝트' },
 ];
+
 export const TeacherSelect = [
   '1학년 담당 선생님',
   '2학년 담당 선생님',
@@ -20,14 +21,34 @@ export const TeacherSelect = [
   '동아리 담당 선생님',
 ];
 
+export type DataIdType = 'web' | 'app' | 'game' | 'embedded' | 'security' | 'aiAndBigData';
+export const FieldListSelect = [
+  { id: 'WEB', content: '웹' },
+  { id: 'APP', content: '앱' },
+  { id: 'GAME', content: '게임' },
+  { id: 'EMBEDDED', content: '임베디드' },
+  { id: 'SECURITY', content: '보안' },
+  { id: 'AI_BIGDATA', content: '인공지능/빅데이터' },
+];
+
 export type ProjectCreateType = {
-  fieldList: [];
-  memberList: UsersListType;
+  fieldList: Array<string>;
+  memberList: Array<MemberListType>;
   projectName: string;
   projectType: string;
   teacher: string;
   teamName: string;
-  techStacks: string;
+  techStack: string;
+};
+
+export type ProjectModifyType = {
+  fieldList: Array<string>;
+  memberList: Array<MemberListType>;
+  projectName: string;
+  projectType: string;
+  teacher: string;
+  teamName: string;
+  techStack: string;
 };
 
 export type MemberListType = {
@@ -44,4 +65,10 @@ export type UsersListType = {
     name: string;
     role: string;
   }>;
+};
+
+export type UrlType = {
+  docsUrl: string;
+  githubUrl: string;
+  serviceUrl: string;
 };
