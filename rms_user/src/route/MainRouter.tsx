a430a42f7fe6b14fd41e92354cb544b0846e89b2
yelim <choyr2016@naver.com>
2021-11-20 23:26:19 +0900

    ⚡️ :: (#29) write route 수정

--- rms_user/src/route/MainRouter.tsx
+++ rms_user/src/route/MainRouter.tsx
@@ -20,8 +20,8 @@ const MainRouter = () => {
         <Route exact path='/' component={LoginContainer} />
         <Route exact path='/auth/token' component={LoginLoadingContainer} />
         <Route exact path='/mypage' component={MyPageContainer} />
-        <Route exact path='/write/plan' component={WritePlanContainer} />
-        <Route exact path='/write/report' component={WriteReportContainer} />
+        <Route exact path='/write/plan/:id' component={WritePlanContainer} />
+        <Route exact path='/write/report/:id' component={WriteReportContainer} />
         <Route exact path='/detail-report/:id' component={DetailReportContainer} />
         <Route exact path='/feed' component={MainContainer} />
         <Route exact path='/projectview' component={ProjectViewContainer} />
