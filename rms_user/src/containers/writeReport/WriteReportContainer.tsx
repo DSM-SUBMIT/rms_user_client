b55879f0a395c6b34ea42c31497cad30a894b06d
hyejun <hyejun0608@naver.com>
2021-11-22 10:20:11 +0900

    ⚡️ :: id 저장

--- rms_user/src/containers/writeReport/WriteReportContainer.tsx
+++ rms_user/src/containers/writeReport/WriteReportContainer.tsx
@@ -1,8 +1,16 @@
-import React, { FC, Suspense } from "react";
-import { WriteReport } from "../../components";
-
+import React, { FC, Suspense, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { WriteReport } from '../../components';
+import useWriteReport from '../../util/hooks/writeReport';
 
 const WriteReportContainer: FC = () => {
+  const { setState, state } = useWriteReport();
+  const { id } = useParams<{ id: string }>();
+
+  useEffect(() => {
+    setState.setProjectId(id);
+  }, [id]);
+
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <WriteReport />
